dbc72dba465149693bc5aeb10b47f8d735c417d0
Jay Millena <jay.millena0&@gmail.com>
2023-11-07 22:44:45 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+<body>
+    <a href="add.php">Add Name</a>
+
+    <table>
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Name</td>    
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>John</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Bogart</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Bornok</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
